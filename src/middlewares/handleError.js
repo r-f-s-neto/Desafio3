be0a44415edca1a845ed3fce9d8facb62470efdf
r-f-s-neto <freeshopeletronicos@gmail.com>
2023-04-15 04:57:13 -0300

    feat(handleerror.js): implementation of UnauthorizedError

--- src/middlewares/handleError.js
+++ src/middlewares/handleError.js
@@ -1,8 +1,12 @@
 import { ValidationError } from 'express-validation';
+import { UnauthorizedError } from 'express-jwt';
 
 const handleError = (error, req, res, next) => {
   if (error instanceof ValidationError) {
     return res.status(error.statusCode).json(error.details.body[0].message);
+  }
+  if (error instanceof UnauthorizedError) {
+    return res.status(error.status).json(error.name);
   }
   return res.status(500).json(error);
 };
