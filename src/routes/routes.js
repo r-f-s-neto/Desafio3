c97888f30dc422c5f5f856ccdefb03cd2b65783d
r-f-s-neto <freeshopeletronicos@gmail.com>
2023-04-17 14:47:11 -0300

    feat(routes.js): implementation of authorization on /dashboard/media/:id

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -45,6 +45,6 @@ routes.get(
 routes.get('/dashboard/numero-paciente', dashboardController.numeroPacientes);
 routes.get('/dashboard/numero-psicologo', dashboardController.numeroPsicologos);
 routes.get('/dashboard/media', dashboardController.media);
-routes.get('/dashboard/media/:id', dashboardController.mediaPerPsicologo);
+routes.get('/dashboard/media/:id', auth, dashboardController.mediaPerPsicologo);
 
 export default routes;
